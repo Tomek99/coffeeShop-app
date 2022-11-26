af84747752f29093e99df8749fc10000f5fedc9e
Tomasz Skupien <Szybko872@gmail.com>
2022-11-26 12:38:29 +0100

    test

--- my-app/src/components/ContactSection.js
+++ my-app/src/components/ContactSection.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import '../styles/ContactSection.scss'
 import Header from './Header'
+
+
 function ContactSection() {
+    const apiCode = 'AIzaSyDbM0YXlgeR75jBGqykBoocBryTXO2Qma4';
+
+
     return (
         <div className='ContactSection' id='contactSection'>
             <Header firstWord="contact" secondWord="us" />
+
         </div>
     )
 }
