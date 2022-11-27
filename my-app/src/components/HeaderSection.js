1017292a2bc7c2e46f25a1d34007a04a72c96b71
Tomasz Skupien <Szybko872@gmail.com>
2022-11-27 18:39:19 +0100

    update

--- my-app/src/components/HeaderSection.js
+++ my-app/src/components/HeaderSection.js
@@ -15,6 +15,7 @@ function Header() {
 
     const unfoldMenu = () => {
         setMenuOpen(!isMenuOpen);
+        console.log(isMenuOpen);
     }
 
 
@@ -22,31 +23,28 @@ function Header() {
         <>
             <div className='divHeader'>
                 <div className='menuHamburger'><button type='button' onClick={unfoldMenu}>X</button></div>
-                <div className='bar'>
+                <div className='bar' >
                     <div className='logo'>
                         <a href="#homeSection"><img src={Logo} alt="" /></a>
                     </div>
-                    {!isMenuOpen ?
-                        <>
-                            <div>
-                                <ul className='navBar'>
-                                    <li><a href='#homeSection'>Home</a></li>
-                                    <li><a href='#aboutSection'>About</a></li>
-                                    <li><a href='#menuSection'>Menu</a></li>
-                                    <li><a href='#productsSection'>Products</a></li>
-                                    <li><a href='#reviewSection'>Review</a></li>
-                                    <li><a href='#contactSection'>Contact</a></li>
-                                    <li><a href='#blogsSection'>Blogs</a></li>
-                                </ul>
-                            </div>
+                    <div className="hideBar" style={{ display: isMenuOpen ? 'none' : 'block' }}>
+                        <ul className='navBar'>
+                            <li><a href='#homeSection'>Home</a></li>
+                            <li><a href='#aboutSection'>About</a></li>
+                            <li><a href='#menuSection'>Menu</a></li>
+                            <li><a href='#productsSection'>Products</a></li>
+                            <li><a href='#reviewSection'>Review</a></li>
+                            <li><a href='#contactSection'>Contact</a></li>
+                            <li><a href='#blogsSection'>Blogs</a></li>
+                        </ul>
+                    </div>
 
-                            <div className='buttonsNextToEO'>
+                    <div className='buttonsNextToEO' style={{ display: isMenuOpen ? 'none' : 'block' }}>
 
-                                <button type="button" onClick={handleSearch}><BsSearch size={30} color={"#fff"} onMouseOver={({ target }) => target.style.color = "d3ad7f"} onMouseOut={({ target }) => target.style.color = "#fff"} /></button>
+                        <button type="button" onClick={handleSearch}><BsSearch size={30} color={"#fff"} onMouseOver={({ target }) => target.style.color = "d3ad7f"} onMouseOut={({ target }) => target.style.color = "#fff"} /></button>
 
-                                <button type="button"><BsCartFill size={30} color={"#fff"} onMouseOver={({ target }) => target.style.color = "d3ad7f"} onMouseOut={({ target }) => target.style.color = "#fff"} /></button>
-                            </div>
-                        </> : null}
+                        <button type="button"><BsCartFill size={30} color={"#fff"} onMouseOver={({ target }) => target.style.color = "d3ad7f"} onMouseOut={({ target }) => target.style.color = "#fff"} /></button>
+                    </div>
                 </div>
             </div>
 
