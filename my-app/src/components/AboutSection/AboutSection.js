2f5d2c7cbb07a4c46dccf9ac77d175d882ea95bd
Tomasz Skupien <Szybko872@gmail.com>
2022-11-29 18:02:01 +0100

    scss corection

--- my-app/src/components/AboutSection/AboutSection.js
+++ my-app/src/components/AboutSection/AboutSection.js
@@ -8,7 +8,7 @@ function AboutSection() {
             <Header firstWord="about" secondWord="us" />
             <div className='div-row'>
                 <img src="/images/about-img.jpeg" alt="AboutImg" />
-                <div>
+                <div className='textArea'>
                     <h1>what makes our coffee special?</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit, Sed Do Eiusmod Tempor Incididunt Ut Labore Et Dolore Magna Aliqua. Ut Enim Ad Minim Veniam, Quis Nostrud Exercitation Ullamco Laboris Nisi Ut Aliquip.
                     </p>
