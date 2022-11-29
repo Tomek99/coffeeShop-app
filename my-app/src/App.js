b16a2efe6ca2ddf52141fa249b4e6604c03d8c71
Tomasz Skupien <Szybko872@gmail.com>
2022-11-29 15:42:47 +0100

    folders added to components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,5 @@
 import './App.css';
-import HeaderSection from './components/HeaderSection/HeaderSection';
-import HomeSection from './components/HomeSection/HomeSection';
-import AboutSection from './components/AboutSection/AboutSection';
-import MenuSection from './components/MenuSection/MenuSection';
-import ProductsSection from './components/ProductsSection/ProductsSection';
-import ReviewSection from './components/ReviewSection/ReviewSection';
-import ContactSection from './components/ContactSection/ContactSection';
-import BlogsSection from './components/BlogsSection/BlogsSection';
-import FooterSection from './components/FooterSection/FooterSection';
+import { HeaderSection, HomeSection, AboutSection, MenuSection, ProductsSection, ReviewSection, ContactSection, BlogsSection, FooterSection } from './components/index'
 
 function App() {
     return (
