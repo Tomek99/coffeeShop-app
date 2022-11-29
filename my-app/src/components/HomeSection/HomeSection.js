731a7bf14b8e14cf57426d383640fec5db8960d4
Tomasz Skupien <Szybko872@gmail.com>
2022-11-29 19:29:07 +0100

    demo version

--- my-app/src/components/HomeSection/HomeSection.js
+++ my-app/src/components/HomeSection/HomeSection.js
@@ -4,7 +4,7 @@ import '../../styles/HomeSection.scss'
 function HomeSection() {
     return (
         <div className='HomeSection' id='homeSection'>
-            <div>
+            <div className='textAreaHome'>
                 <header>fresh coffee in <br />the morning</header>
                 <p>Lorem ipsum Dolor Sit Amet Consectetur Adipisicing Elit. Sit,<br /> Consequuntur? Et Veritatis Velit Nesciunt Odio!</p>
                 <button type='button'>Get It Now</button>
