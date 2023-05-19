96a61b55672db6b91d26ccd771fd9820a17bb33a
tomek <tomaszskupien06@gmail.com>
2023-05-19 20:14:51 +0200

    carouselArticles added

--- client/src/components/BlogComponents/CarouselArticles/CarouselArticles.jsx
+++ client/src/components/BlogComponents/CarouselArticles/CarouselArticles.jsx
@@ -1,8 +1,61 @@
 import React from "react";
 import styles from "./CarouselArticles.module.scss";
+import CarouselArticle from "./CarouselArticle/CarouselArticle";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import "./styles.css";
+
+// import required modules
 
 function CarouselArticles() {
-  return <div className={styles.CarouselArticles}>CarouselArticles</div>;
+  return (
+    <div className={styles.CarouselArticles}>
+      <Swiper
+        slidesPerView={"auto"}
+        grabCursor={true}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        className="mySwiper"
+      >
+        <SwiperSlide style={{ width: "auto" }}>
+          {" "}
+          <CarouselArticle />
+        </SwiperSlide>
+        <SwiperSlide style={{ width: "auto" }}>
+          {" "}
+          <CarouselArticle />
+        </SwiperSlide>
+        <SwiperSlide style={{ width: "auto" }}>
+          {" "}
+          <CarouselArticle />
+        </SwiperSlide>
+        <SwiperSlide style={{ width: "auto" }}>
+          {" "}
+          <CarouselArticle />
+        </SwiperSlide>
+        <SwiperSlide style={{ width: "auto" }}>
+          {" "}
+          <CarouselArticle />
+        </SwiperSlide>
+        <SwiperSlide style={{ width: "auto" }}>
+          {" "}
+          <CarouselArticle />
+        </SwiperSlide>
+        <SwiperSlide style={{ width: "auto" }}>
+          {" "}
+          <CarouselArticle />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 }
 
 export default CarouselArticles;
