77857c47b4a161f9a59766508df8beee68dcd809
tomek <tomaszskupien06@gmail.com>
2024-01-04 17:21:34 +0100

    checkoutInvalidData Test completed

--- client/Playwright/tests/checkoutInvalidDataTest.spec.ts
+++ client/Playwright/tests/checkoutInvalidDataTest.spec.ts
@@ -1,26 +1,76 @@
-// import { test, expect } from "@playwright/test";
-// import { BrowserstackHomePage } from "../pages/homePage";
-// import { ProductsPage } from "../pages/productsPage";
-// import { ViewCartPage } from "../pages/viewCartPage";
-// import { LoginPage } from "../pages/loginPage";
-// import { OrderPage } from "../pages/orderPage";
+import { test, expect } from "@playwright/test";
+import { BrowserstackHomePage } from "../pages/homePage";
+import { ProductsPage } from "../pages/productsPage";
+import { ViewCartPage } from "../pages/viewCartPage";
+import { LoginPage } from "../pages/loginPage";
+import { OrderPage } from "../pages/orderPage";
 
-// import { BaseTest } from "./BaseTest";
+import { BaseTest } from "./BaseTest";
+import { deliveryAddressData } from "../data/deliveryAddressData";
+import { OrderSummaryPage } from "../pages/orderSummaryPage";
+import { StripePage } from "../pages/stripePage";
+import { SuccessPage } from "../pages/successPage";
 
-// test.describe("Purchase products with invalid data", () => {
-//   test.beforeEach(async ({ page }) => {
-//     BaseTest.performBasicStepsForCheckout(page);
-//   });
+test.describe("Purchase products with invalid data", () => {
+  test.beforeEach(async ({ page }) => {
+    BaseTest.performBasicStepsForCheckout(page);
+  });
 
-//   test("place an order as private person", async ({ page }) => {});
+  test("skip filling delivery and payment form", async ({ page }) => {
+    const orderPage = new OrderPage(page);
+    await orderPage.clickOnSummaryBtn();
 
-//   test("place an order as company", async ({ page }) => {});
+    const errors = await orderPage.getErrors();
+    await expect(errors).toHaveCount(10);
 
-//   test("place an order with empty data", async ({ page }) => {});
+    await expect(errors.nth(0)).toContainText("Required");
+  });
 
-//   test("provide other invoice details", async ({ page }) => {});
+  test("skip ticking delivery form", async ({ page }) => {
+    const orderPage = new OrderPage(page);
+    await orderPage.clickOnPrivatePerson();
+    await orderPage.fillDeliveryAddressForm(deliveryAddressData);
+    await orderPage.clickOnlinePayment();
+    await orderPage.clickOnSummaryBtn();
 
-//   test("edit data in order summary", async ({ page }) => {});
+    const errors = await orderPage.getErrors();
+    await expect(errors).toHaveCount(1);
+    await expect(errors).toContainText("Required");
+  });
 
-//   test("add comment to order", async ({ page }) => {});
-// });
+  test("skip ticking 'purchasking as' form", async ({ page }) => {
+    const orderPage = new OrderPage(page);
+    await orderPage.clickOnCarrier();
+    await orderPage.fillDeliveryAddressForm(deliveryAddressData);
+    await orderPage.clickOnlinePayment();
+    await orderPage.clickOnSummaryBtn();
+
+    const errors = await orderPage.getErrors();
+    await expect(errors).toHaveCount(1);
+    await expect(errors).toContainText("Required");
+  });
+
+  test("skip filling delivery address form", async ({ page }) => {
+    const orderPage = new OrderPage(page);
+    await orderPage.clickOnCarrier();
+    await orderPage.clickOnPrivatePerson();
+    await orderPage.clickOnlinePayment();
+    await orderPage.clickOnSummaryBtn();
+
+    const errors = await orderPage.getErrors();
+    await expect(errors).toHaveCount(7);
+    await expect(errors.nth(5)).toContainText("Required");
+  });
+
+  test("skip ticking payment method form", async ({ page }) => {
+    const orderPage = new OrderPage(page);
+    await orderPage.clickOnCarrier();
+    await orderPage.clickOnPrivatePerson();
+    await orderPage.fillDeliveryAddressForm(deliveryAddressData);
+    await orderPage.clickOnSummaryBtn();
+
+    const error = await orderPage.getErrors();
+    await expect(error).toHaveCount(1);
+    await expect(error).toContainText("Required");
+  });
+});
