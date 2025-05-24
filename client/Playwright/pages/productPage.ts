83445e3217e64640ea4c73a6be7e7a1b3b521273
tomek <tomaszskupien06@gmail.com>
2025-05-24 15:07:59 +0200

    2 tests completed

--- client/Playwright/pages/productPage.ts
+++ client/Playwright/pages/productPage.ts
@@ -2,8 +2,33 @@ import { Locator, Page } from "@playwright/test";
 
 export class ProductPage {
   private readonly page: Page;
+  private readonly productReviewsBtn: Locator;
+  private readonly thumbUpBtn: Locator;
+  private readonly thumDownBtn: Locator;
 
   constructor(page: Page) {
     this.page = page;
+    this.productReviewsBtn = page.getByRole("button", {
+      name: /Product reviews/i,
+    });
+
+    this.thumbUpBtn = page
+      .locator("//button[@class='BtnThumbs_thumb__DhYHG']")
+      .first();
+    this.thumDownBtn = page
+      .locator("//button[@class='BtnThumbs_thumb__DhYHG']")
+      .nth(1);
+  }
+
+  async clickOnProductReviews() {
+    await this.productReviewsBtn.click();
+  }
+
+  async clickOnThumbUp() {
+    await this.thumbUpBtn.click();
+  }
+
+  async clickOnThumbDown() {
+    await this.thumDownBtn.click();
   }
 }
--- client/Playwright/tests/reviewThumbTest.spec.ts
+++ client/Playwright/tests/reviewThumbTest.spec.ts
@@ -1,23 +1,22 @@
 import { test, expect, Page } from "@playwright/test";
 import { ApiTests } from "./ApiTests";
 import { BrowserstackHomePage } from "../pages/homePage";
+import { ProductsPage } from "../pages/productsPage";
+import { ProductPage } from "../pages/productPage";
 
 const userId: string = "64ad79b0111e32cf0a6846e9";
 let reviewId: string;
 
 test.describe("Thumb up and down when user is logged in", () => {
-  test.beforeEach(async ({ page }) => {
-    reviewId = await ApiTests.addCompletedReviewProduct(page, userId);
-  });
+  // test.beforeEach(async ({ page }) => {
+  //   reviewId = await ApiTests.addCompletedReviewProduct(page, userId);
+  // });
 
-  test.afterEach(async ({ page }) => {
-    await ApiTests.deleteReviewProduct(page, reviewId);
-  });
+  // test.afterEach(async ({ page }) => {
+  //   await ApiTests.deleteReviewProduct(page, reviewId);
+  // });
 
-  test("should allow thumb up", async ({ page }) => {
-    const homePage = await new BrowserstackHomePage(page);
-    await homePage.goToHomePage();
-  });
+  test("should allow thumb up", async ({ page }) => {});
 
   test("should allow thumb down", async ({ page }) => {});
 
@@ -27,7 +26,37 @@ test.describe("Thumb up and down when user is logged in", () => {
 });
 
 test.describe("Thumb up and down when user is NOT logged in", () => {
-  test("should NOT allow thumb up", async ({ page }) => {});
+  test("should NOT allow thumb up", async ({ page }) => {
+    const homePage = await new BrowserstackHomePage(page);
+    const productsPage = await new ProductsPage(page);
+    const productPage = await new ProductPage(page);
 
-  test("should NOT allow thumb down", async ({ page }) => {});
+    await homePage.goToHomePage();
+    await homePage.clickOnProductsBtn();
+    await productsPage.clickOnViewProductBtn(7);
+    await productPage.clickOnProductReviews();
+    await productPage.clickOnThumbUp();
+
+    const locator = await page.locator(
+      "//div[contains(text(), 'You cannot vote if you are not logged in.')]"
+    );
+    await expect(locator).toBeVisible();
+  });
+
+  test("should NOT allow thumb down", async ({ page }) => {
+    const homePage = await new BrowserstackHomePage(page);
+    const productsPage = await new ProductsPage(page);
+    const productPage = await new ProductPage(page);
+
+    await homePage.goToHomePage();
+    await homePage.clickOnProductsBtn();
+    await productsPage.clickOnViewProductBtn(7);
+    await productPage.clickOnProductReviews();
+    await productPage.clickOnThumbDown();
+
+    const locator = await page.locator(
+      "//div[contains(text(), 'You cannot vote if you are not logged in.')]"
+    );
+    await expect(locator).toBeVisible();
+  });
 });
